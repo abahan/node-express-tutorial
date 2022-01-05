0d10be23636b72b45ed1d657795388dd4a36aa43
Alex Bahan <43921679+abahan@users.noreply.github.com>
2022-01-04 20:01:58 -0500

    post   JSON info

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 const express = require('express');
+const shortid = require('shortid');
 
-const server = express();
+const app = express();
+app.use(express.json());
 const port = 5000;
 
-server.listen(port, () => console.log(`\n*** Server running on port ${port}`));
+const channels = [];
+const lessons = [];
+
+
+app.get('/', (req, res) => {
+   
+    res.json({hello:"world"});
+});
+
+app.post('/api/channels', (req, res) => {
+   
+    const channelInfo = req.body;
+    channelInfo.id = shortid.generate();
+    channels.push(channelInfo);
+    res.status(201).json(channels);
+
+});
+
+app.get('/hello', (req, res) => {
+    res.json({hello:"future visitor"});
+});
+
+app.listen(port, () => console.log(`\n*** Server running on port ${port}`));
